2ef00d2dd4fd853806e6bb3e73afa3f95858e53f
mufaddal1125 <mufaddal1125@gmail.com>
2021-03-08 10:18:47 +0400

    push

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SideBar from './SideBar'
 import SetImage from './SetImage'
 // import Image from './Image'
 import URLImage from './URLImage'
-import {Stage, Layer} from 'react-konva'
+import { Stage, Layer } from 'react-konva'
 // import Canvas from './Canvas'
 
 
@@ -98,9 +98,9 @@ function App() {
   const [selectedOptionsIndex, setSelectedOptionsIndex] = useState(0)
   const [options, setOptions] = useState(DEFAULT_OPTIONS)
   const selectedOption = options[selectedOptionsIndex]
-  
+
   const [src, setSrc] = useState('https://images.unsplash.com/photo-1480506132288-68f7705954bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1093&q=80')
-  
+
 
   function handleSliderChange({ target }) {
     setOptions(prevOptions => {
@@ -110,36 +110,34 @@ function App() {
       })
     })
 
-    
+
   }
 
   // function getImageStyle(params) {
   //   const filters = options.map(option => {
   //     return `${options.value}`
   //   })
-    
+
   //   return {"style": {filter: filters.join(' ')}, blur: filters[6]}
   // }
-  
-  
 
-  function onSetImageHandler(url){
+
+  
+  function onSetImageHandler(url) {
     setSrc(url)
   }
-  
-  
+
+
   return (
     <div className="container">
       <div className="main-image">
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer>
-          
-        <URLImage noise={options[5].value / 1} posterize={options[4].value / 1} pixel={options[2].value / 1} luminance={options[3].value / 1} contrast={options[1].value / 1} brightness={options[0].value / 10} Blur={options[6].value} src={src}></URLImage>
-          
-        </Layer>
-      </Stage>
+        <Stage width={window.innerWidth} height={window.innerHeight}>
+          <Layer className="image-parent">
+            <URLImage className="image" noise={options[5].value / 1} posterize={options[4].value / 1} pixel={options[2].value / 1} luminance={options[3].value / 1} contrast={options[1].value / 1} brightness={options[0].value / 10} Blur={options[6].value} src={src}></URLImage>
+          </Layer>
+        </Stage>
       </div>
-      
+
       <div className="side-bar">
         {options.map((option, index) => {
           return (<SideBar
@@ -150,7 +148,7 @@ function App() {
           />
           )
         })}
-        <SetImage 
+        <SetImage
           onset={onSetImageHandler}
         />
       </div>
