6fd2a673823a2286e21a6eaa3570207227dc4faf
mufaddal <mufaddal1125@gmail.com>
2021-07-04 22:41:51 +0400

    added download button fix height and width issue, remove sidebar button underlinse

--- src/App.js
+++ src/App.js
@@ -1,166 +1,185 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import './App.css'
 import Slider from './Slider.js'
 import SideBar from './SideBar'
 import SetImage from './SetImage'
-// import Image from './Image'
 import URLImage from './URLImage'
-import { Stage, Layer } from 'react-konva'
-// import Canvas from './Canvas'
+import {Stage, Layer} from 'react-konva'
 
 
 const DEFAULT_OPTIONS = [
-  {
-    name: "Brightness",
-    property: 'brightness',
-    value: 1,
-    range: {
-      min: 0,
-      max: 2
+    {
+        name: "Brightness",
+        property: 'brightness',
+        value: 1,
+        range: {
+            min: 0,
+            max: 2
+        },
+        unit: '%',
+        step: 0.1
     },
-    unit: '%',
-    step: 0.1
-  },
-  {
-    name: "Contrast",
-    property: 'contrast',
-    value: 10,
-    range: {
-      min: -20,
-      max: 20
+    {
+        name: "Contrast",
+        property: 'contrast',
+        value: 10,
+        range: {
+            min: -20,
+            max: 20
+        },
+        unit: '%',
+        step: 0.5
     },
-    unit: '%',
-    step: 0.5
-  },
-  {
-    name: "Pixelate",
-    property: 'pixel',
-    value: 0.1,
-    range: {
-      min: 0.1,
-      max: 10
-    },
-    unit: '%',
-    step: 0.1
+    {
+        name: "Pixelate",
+        property: 'pixel',
+        value: 0.1,
+        range: {
+            min: 0.1,
+            max: 10
+        },
+        unit: '%',
+        step: 0.1
 
-  },
-  {
-    name: "Luminance",
-    property: 'luminance',
-    value: 0,
-    range: {
-      min: -1,
-      max: 1
     },
-    unit: '%',
-    step: 0.1
+    {
+        name: "Luminance",
+        property: 'luminance',
+        value: 0,
+        range: {
+            min: -1,
+            max: 1
+        },
+        unit: '%',
+        step: 0.1
 
-  },
-  {
-    name: "Posterize",
-    property: 'posterize',
-    value: 0.1,
-    range: {
-      min: 0,
-      max: 0.1
     },
-    unit: '%',
-    step: 0.01
-  },
-  {
-    name: "Noise",
-    property: 'noise',
-    value: 0,
-    range: {
-      min: 0,
-      max: 2
+    {
+        name: "Posterize",
+        property: 'posterize',
+        value: 0.1,
+        range: {
+            min: 0,
+            max: 0.1
+        },
+        unit: '%',
+        step: 0.01
     },
-    unit: 'deg',
-    step: 0.1
-  },
-  {
-    name: "Blur",
-    property: 'blur',
-    value: 0,
-    range: {
-      min: 0,
-      max: 20
+    {
+        name: "Noise",
+        property: 'noise',
+        value: 0,
+        range: {
+            min: 0,
+            max: 2
+        },
+        unit: 'deg',
+        step: 0.1
     },
-    unit: 'px',
-    step: 1
-  },
+    {
+        name: "Blur",
+        property: 'blur',
+        value: 0,
+        range: {
+            min: 0,
+            max: 20
+        },
+        unit: 'px',
+        step: 1
+    },
 ]
 
 
-
-
 function App() {
-  const [selectedOptionsIndex, setSelectedOptionsIndex] = useState(0)
-  const [options, setOptions] = useState(DEFAULT_OPTIONS)
-  const selectedOption = options[selectedOptionsIndex]
-
-  const [src, setSrc] = useState('https://images.unsplash.com/photo-1480506132288-68f7705954bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1093&q=80')
-
-
-  function handleSliderChange({ target }) {
-    setOptions(prevOptions => {
-      return prevOptions.map((option, index) => {
-        if (index !== selectedOptionsIndex) { return option }
-        return { ...option, value: target.value }
-      })
-    })
+    const [selectedOptionsIndex, setSelectedOptionsIndex] = useState(0)
+    const [options, setOptions] = useState(DEFAULT_OPTIONS)
+    const selectedOption = options[selectedOptionsIndex]
+    const [src, setSrc] = useState(`https://picsum.photos/${1280}/${window.innerHeight}`)
 
 
-  }
-
-  // function getImageStyle(params) {
-  //   const filters = options.map(option => {
-  //     return `${options.value}`
-  //   })
-
-  //   return {"style": {filter: filters.join(' ')}, blur: filters[6]}
-  // }
-
-
-  
-  function onSetImageHandler(url) {
-    setSrc(url)
-  }
+    function handleSliderChange({target}) {
+        setOptions(prevOptions => {
+            return prevOptions.map((option, index) => {
+                if (index !== selectedOptionsIndex) {
+                    return option
+                }
+                return {...option, value: target.value}
+            })
+        })
 
 
-  return (
-    <div className="container">
-      <div className="main-image">
-        <Stage width={window.innerWidth} height={window.innerHeight}>
-          <Layer className="image-parent">
-            <URLImage className="image" noise={options[5].value / 1} posterize={options[4].value / 1} pixel={options[2].value / 1} luminance={options[3].value / 1} contrast={options[1].value / 1} brightness={options[0].value / 10} Blur={options[6].value} src={src}></URLImage>
-          </Layer>
-        </Stage>
-      </div>
+    }
 
-      <div className="side-bar">
-        {options.map((option, index) => {
-          return (<SideBar
-            key={index}
-            name={option.name}
-            active={index === selectedOptionsIndex}
-            handleClick={() => { setSelectedOptionsIndex(index) }}
-          />
-          )
-        })}
-        <SetImage
-          onset={onSetImageHandler}
-        />
-      </div>
-      <Slider
-        min={selectedOption.range.min}
-        max={selectedOption.range.max}
-        value={selectedOption.value}
-        step={selectedOption.step}
-        handleChange={handleSliderChange}
-      />
-    </div>
-  );
+    // function getImageStyle(params) {
+    //   const filters = options.map(option => {
+    //     return `${options.value}`
+    //   })
+
+    //   return {"style": {filter: filters.join(' ')}, blur: filters[6]}
+    // }
+
+
+    function onSetImageHandler(url) {
+        setSrc(url)
+    }
+
+    const stageRef = React.useRef(null);
+    function downloadURI(uri, name) {
+        let link = document.createElement('a');
+        link.download = name;
+        link.href = uri;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+    const handleExport = () => {
+        const uri = stageRef.current.toDataURL();
+        // window.open(uri, '_blank');
+        // we also can save uri as file
+        // but in the demo on Konva website it will not work
+        // because of iframe restrictions
+        // but feel free to use it in your apps:
+        downloadURI(uri, 'image.png');
+    };
+    return (
+        <div className="container">
+            <div className="main-image">
+                <Stage width={window.innerWidth} height={window.innerHeight} ref={stageRef} >
+                    <Layer className="image-parent">
+                        <URLImage className="image" noise={options[5].value} posterize={options[4].value}
+                                  pixel={options[2].value} luminance={options[3].value}
+                                  contrast={options[1].value} brightness={options[0].value / 10}
+                                  Blur={options[6].value} src={src}/>
+                    </Layer>
+                </Stage>
+            </div>
+
+            <div className="side-bar">
+                {options.map((option, index) => {
+                    return (<SideBar
+                            key={index}
+                            name={option.name}
+                            active={index === selectedOptionsIndex}
+                            handleClick={() => {
+                                setSelectedOptionsIndex(index)
+                            }}
+                        />
+                    )
+                })}
+                <SetImage
+                    onset={onSetImageHandler}
+                />
+                <button className="set-image-button" onClick={handleExport}>Download Image</button>
+            </div>
+            <Slider
+                min={selectedOption.range.min}
+                max={selectedOption.range.max}
+                value={selectedOption.value}
+                step={selectedOption.step}
+                handleChange={handleSliderChange}
+            />
+        </div>
+    );
 }
 
 export default App;
--- src/SetImage.js
+++ src/SetImage.js
@@ -5,9 +5,9 @@ export default function SetImage({onset}) {
     const [url, setUrl] = useState('')
 
     return (
-        <div className="set-image">
+        <div className="set-image" style={{height: '100%'}}>
             <h4>Set Image:</h4>
-            <input type="url" className="set-image-url" onChange={(event) => setUrl(event.target.value)} placeholder="Paste Image URL Here"></input>
+            <input type="url" className="set-image-url" onChange={(event) => setUrl(event.target.value)} placeholder="Paste Image URL Here"/>
             <button className="set-image-button" onClick={() => onset(url)}>Set Image</button>
         </div>
     )
